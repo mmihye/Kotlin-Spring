f999862c016db98624bd08e796f6c0bb5e606e6e
mmihye <smilehye910@naver.com>
2024-08-18 00:11:16 +0900

    Feat : BaseEntity 추가

---
+++ src/main/kotlin/com/example/payment/domain/BaseEntity.kt
@@ -0,0 +1,22 @@
+package com.example.payment.domain
+
+import jakarta.persistence.*
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
+    @CreatedDate
+    lateinit var createdDate:LocalDateTime
+    @LastModifiedDate
+    lateinit var updatedAt:LocalDateTime
+
+
+}
